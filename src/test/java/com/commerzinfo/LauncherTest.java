f2c50cdb545f3239af11cf59da3f55ec307b2a8e
dergrafiker <stephan.herder@gmail.com>
2014-06-30 14:06:10 +0200

    ignore test

--- src/test/java/com/commerzinfo/LauncherTest.java
+++ src/test/java/com/commerzinfo/LauncherTest.java
@@ -1,6 +1,7 @@
 package com.commerzinfo;
 
 import junit.framework.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -10,6 +11,7 @@ import java.util.List;
 
 public class LauncherTest {
 
+    @Ignore
     @Test
     public void testMain() throws Exception {
         List<String> args = new ArrayList<String>();
