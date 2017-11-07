acf6d34194561959e786c34d92ad2ee3613ff7ce
dergrafiker <stephan.herder@gmail.com>
2017-11-07 09:44:32 +0100

    added tests

---
+++ src/test/java/com/commerzinfo/input/csv/CSVParserTest.java
@@ -0,0 +1,23 @@
+package com.commerzinfo.input.csv;
+
+import com.commerzinfo.DataRow;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CSVParserTest {
+    @Test
+    public void handleCSV() throws Exception {
+        File inputFile = Paths.get("src/test/resources/testdata.CSV").toFile();
+        List<DataRow> dataRows = CSVParser.handleCSV(inputFile);
+
+        Assert.assertThat(dataRows.size(), is(4));
+    }
+
+}
---
+++ src/test/java/com/commerzinfo/input/html/HTMLParserTest.java
@@ -0,0 +1,13 @@
+package com.commerzinfo.input.html;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HTMLParserTest {
+    @Test
+    public void handleHTML() throws Exception {
+
+    }
+
+}
