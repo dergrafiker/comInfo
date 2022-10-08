b2afdbedc4ece5752bd7b8fd8e854b760d32ba11
dergrafiker <stephan.herder@gmail.com>
2022-10-08 22:33:26 +0200

    replaced opencsv with fastcsv

--- src/main/java/com/commerzinfo/input/csv/CSVParser.java
+++ src/main/java/com/commerzinfo/input/csv/CSVParser.java
@@ -4,9 +4,7 @@ import com.commerzinfo.DataRow;
 import com.commerzinfo.util.CompressionUtil;
 import com.commerzinfo.util.DateUtil;
 import com.commerzinfo.util.DecimalFormatUtil;
-import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
-import com.opencsv.bean.MappingStrategy;
+import de.siegmar.fastcsv.reader.NamedCsvReader;
 import org.apache.commons.io.input.BOMInputStream;
 import org.tinylog.Logger;
 
@@ -14,54 +12,33 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class CSVParser {
-    private static final MappingStrategy<CSVBean> mappingStrategy = initStrategy();
-    private static final char SEPARATOR = ';';
-    private static final char QUOTECHAR = '"';
-
-    private static HeaderColumnNameTranslateMappingStrategy<CSVBean> initStrategy() {
-        final HeaderColumnNameTranslateMappingStrategy<CSVBean> translateMappingStrategy = new HeaderColumnNameTranslateMappingStrategy<>();
-        translateMappingStrategy.setType(CSVBean.class);
-        Map<String, String> map = new HashMap<>();
-        map.put("Buchungstag", "buchungstag");
-        map.put("Wertstellung", "wertstellung");
-        map.put("Buchungstext", "buchungstext");
-        map.put("Betrag", "betrag");
-        map.put("Währung", "waehrung");
-        translateMappingStrategy.setColumnMapping(map);
-        return translateMappingStrategy;
-    }
 
     public static List<DataRow> handleCSV(File file) throws IOException {
         List<DataRow> dataRows = new ArrayList<>();
         try (InputStream inputStream = CompressionUtil.getCorrectInputStream(file)) {
             try (BOMInputStream bomInputStream = new BOMInputStream(inputStream, false)) {
                 try (InputStreamReader inputStreamReader = new InputStreamReader(bomInputStream, "UTF-8")) {
-                    List<CSVBean> csvBeanList = new CsvToBeanBuilder<CSVBean>(inputStreamReader)
-                            .withSeparator(SEPARATOR)
-                            .withQuoteChar(QUOTECHAR)
-                            .withMappingStrategy(mappingStrategy)
-                            .build()
-                            .parse();
-
-                    for (CSVBean csvBean : csvBeanList) {
-                        try {
-                            DataRow row = new DataRow();
-                            row.setBookingDate(DateUtil.parse(csvBean.getBuchungstag()));
-                            row.setValueDate(DateUtil.parse(csvBean.getWertstellung()));
-                            row.setBookingText(csvBean.getBuchungstext());
-                            row.setValue((java.math.BigDecimal) DecimalFormatUtil.parse(csvBean.getBetrag(),
-                                                                                        DecimalFormatUtil.Mode.CSV));
-                            dataRows.add(row);
-                        } catch (ParseException e) {
-                            Logger.error("problem with datarow mapping", e);
-                        }
+                    try (NamedCsvReader csvReader = NamedCsvReader.builder()
+                            .fieldSeparator(';')
+                            .quoteCharacter('"')
+                            .build(inputStreamReader)) {
+                        csvReader.forEach(csvRow -> {
+                            try {
+                                DataRow row = new DataRow();
+                                row.setBookingDate(DateUtil.parse(csvRow.getField("Buchungstag")));
+                                row.setValueDate(DateUtil.parse(csvRow.getField("Wertstellung")));
+                                row.setBookingText(csvRow.getField("Buchungstext"));
+                                row.setValue((java.math.BigDecimal) DecimalFormatUtil.parse(csvRow.getField("Betrag"),
+                                        DecimalFormatUtil.Mode.CSV));
+                                dataRows.add(row);
+                            } catch (Exception e) {
+                                Logger.error("problem with datarow mapping", e);
+                            }
+                        });
                     }
                 }
             }
